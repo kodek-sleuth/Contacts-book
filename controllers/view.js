312be8f3077ee5fe47b99b5b0c3a9b7481cf8689
ele <elemanhillary@gmail.com>
2019-06-16 19:10:13 +0200

    :sparkles: ft-return-helper adds returnHelper function

--- controllers/view.js
+++ controllers/view.js
@@ -8,3 +8,28 @@ import Contact from '../models/contacts.model';
 const handleError = (err) => {
   console.error(err);
 };
+
+/**
+ * @function returnHelper
+ * @param {Error} err
+ * @param {Object} res
+ * @param {Object} contact
+ * @returns {Object}
+ */
+
+export const returnHelper = (err, res, contact) => {
+  if (err) return handleError(err);
+  if (contact) {
+    return res.status(200).json({ status: 200, data: contact });
+  }
+  return res.status(404).json({ status: 404, error: 'no content found' });
+};import Contact from '../models/contacts.model';
+
+/**
+ * @function handleError
+ * @param {Error} err
+ */
+
+const handleError = (err) => {
+  console.error(err);
+};
