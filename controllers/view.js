e508e0a72c61301e111bc340e226fd01c8e0df17
kliques <elemanhillary@gmail.com>
2019-06-16 21:27:31 +0200

    Delete view.js

--- controllers/view.js
+++ controllers/view.js
@@ -8,3 +8,19 @@ import Contact from '../models/contacts.model';
 const handleError = (err) => {
   console.error(err);
 };
+
+/**
+ * @function returnHelper
+ * @param {Error} err
+ * @param {Object} res
+ * @param {Object} contact
+ * @returns {Object}
+ */
+
+export const returnHelper = (err, res, contact) => {
+  if (err) return handleError(err);
+  if (contact) {
+    return res.status(200).json({ status: 200, data: contact });
+  }
+  return res.status(404).json({ status: 404, error: 'no content found' });
+};
