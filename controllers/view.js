b8dc48f722a2a1cb7f97cd74126d94cac9737a39
ele <elemanhillary@gmail.com>
2019-06-16 19:38:51 +0200

    :sparkles: ft-view-all

--- controllers/view.js
+++ controllers/view.js
@@ -24,3 +24,17 @@ export const returnHelper = (err, res, contact) => {
   }
   return res.status(404).json({ status: 404, error: 'no content found' });
 };
+
+/**
+ * @function viewAll
+ * @param {Object} res
+ * @returns {Object}
+ */
+
+export const viewAll = (_req, res) => {
+  Contact.find({})
+    .select(['-__v'])
+    .exec((err, contact) => {
+      returnHelper(err, res, contact);
+    });
+};
