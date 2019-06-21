870c5598374b27834e64eac94939d058c0133ada
Celestin NIYONSABA <niyoceles3@gmail.com>
2019-06-21 09:25:38 +0200

    Update readme and router

--- app/routes/contanctRouter.js
+++ app/routes/contanctRouter.js
@@ -3,7 +3,7 @@ import contactController from '../controllers/contactController';
 
 const router = express.Router();
 router.post('/', contactController.createContact);
-router.patch('/:id', contactController.updateContact);
+router.patch('/:id/update', contactController.updateContact);
 router.delete('/:id/delete', contactController.deleteContact);
 
 export default router;
